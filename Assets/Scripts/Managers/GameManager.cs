19f03f88489f886af06ba36c652cc371917b5041
pollikk <ttc_96@hotmail.no>
2025-10-27 23:03:07 +0100

    Made managers

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public MoneyManager moneyManager {get; set;}
+
+
+    private void Awake(){
+        if(Instance == null){
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }else{
+            Destroy(gameObject);
+        }
+        Application.runInBackground = true;
+    }
+
+    private void Start(){
+        moneyManager = FindFirstObjectByType<MoneyManager>();
+    }
+
+
+    public void SaveGameBTN(){
+        SaveSystem.Save();
+    }
+    public void LoadGameBTN(){
+        SaveSystem.Load();
+    }
+
+    private void Update(){
+        if(Input.GetKeyDown(KeyCode.S)){
+            SaveSystem.Save();
+            print("Saving!");
+        }
+        if(Input.GetKeyDown(KeyCode.L)){
+            SaveSystem.Load();
+            print("Loading!");
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/Managers/SaveSystem/SaveSystem.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SaveSystem
+{
+    private static SaveData _saveData = new SaveData();
+    [System.Serializable]
+    public struct SaveData
+    {
+        public CurrencySaveData currencySaveData;
+
+    }
+
+    public static string SaveFileName()
+    {
+        string saveFile = Application.persistentDataPath + "/idleVillageSave" + ".save";
+        return saveFile;
+    }
+
+    public static void Save()
+    {
+        HandleSaveData();
+
+        File.WriteAllText(SaveFileName(), JsonUtility.ToJson(_saveData, true));
+    }
+    public static void Load()
+    {
+        string saveContent = File.ReadAllText(SaveFileName());
+        _saveData = JsonUtility.FromJson<SaveData>(saveContent);
+
+        HandleLoadData();
+    }
+
+
+    //--------------SETUP FOR SAVING SCRIPT-------------------------
+    // First comment for name of the script thats beign saved
+    // Second comment for function of the script
+
+    public static void HandleSaveData()
+    {
+        GameManager.Instance.moneyManager.Save(ref _saveData.currencySaveData);
+    }
+    
+
+    public static void HandleLoadData()
+    {
+        GameManager.Instance.moneyManager.Load(_saveData.currencySaveData);
+
+    }
+
+}
